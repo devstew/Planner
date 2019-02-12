c532783ed56a47b3306857f56ab41a0f29125c18
devstew <devstew@iMac-Boost.local>
2019-02-12 13:00:11 +0200

    fixed reduxFirestore issue

--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { reduxFirestore, getFirestore} from "redux-firestore";
-import { reactReduxFirebase, getFirebase} from 'react-redux-firebase';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
 import fbConfig from './config/fbConfig';
 
 const store = createStore(rootReducer,
     compose(
         applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore})),
-        reduxFirestore(fbConfig),
-        reduxReduxFirebase(fbConfig)
+        reactReduxFirebase(fbConfig),
+        reduxFirestore(fbConfig)
     )
 );
 
